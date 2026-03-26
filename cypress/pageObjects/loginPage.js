730fadc2b939de77633409885ddd17159c85abb7
kovaadri <s23kovaadri@venta.lv>
2026-03-26 11:22:01 +0200

    Add: init & lecture examples

--- cypress/pageObjects/loginPage.js
+++ cypress/pageObjects/loginPage.js
@@ -8,4 +8,20 @@ export class LoginPage extends BasePage {
   static get elementName() {
     return cy.get("elementSelector");
   }
+
+  static get emailField() {
+    return cy.get("input#email");
+  }
+
+  static get passwordField() {
+    return cy.get("input#password");
+  }
+
+  static get loginButton() {
+    return cy.get("#loginButton");
+  }
+
+  static get notYetCustomerLink() {
+    return cy.get("[routerlink='/register']");
+  }
 }
